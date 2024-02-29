43e575736002373e86e97a2b4000e077f52e7ab6
panwung <panwung@myvuw.ac.nz>
2024-02-29 18:33:27 +1300

    Made OrthoCube drawable
    cubes can now be drawn at different vertical angles

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -1,26 +1,104 @@
 class OrthoCube {
-	constructor(xCenter, yCenter, cubeScale, active=false) {
-	  this.xCenter = xCenter;
-	  this.yCenter = yCenter;
-	  this.cubeScale = cubeScale;
-	  this.active = active;
+	constructor(xCenter, yCenter, gridPos, viewAngleDeg, edgeLength, cubeScale=1, active=false) {
+		this.points = [[
+			xCenter,  // Ax
+			yCenter	  // Ay
+		]];
+
+		this.gridPos = gridPos;
+
+		this.viewAngle = viewAngleDeg * Math.PI / 180; // Conver to Rad
+		this.edgeLength = edgeLength;
+		this.cubeScale = cubeScale;
+	  
+		this.active = active;
+
+		this._buildPoints();
 	}
   
-  
-	draw() {
-	  push();
-	  beginShape();
-  
-	  endShape(CLOSE);
-	  pop();
+	draw(raiseHeight=0) {
+		push();
+		scale(this.cubeScale);
+		translate(this.points[0][0], this.points[0][1]);
+
+		// Draws top rhombus
+		beginShape();
+		vertex(0, 0);								  // A
+		vertex(this.points[1][0], this.points[1][1]); // B
+		vertex(this.points[6][0], this.points[6][1]); // G
+		vertex(this.points[2][0], this.points[2][1]); // C
+		endShape(CLOSE);
+
+		// Draws left rhombus
+		beginShape();
+		vertex(0, 0);								  // A
+		vertex(this.points[1][0], this.points[1][1]); // B
+		vertex(this.points[3][0], this.points[3][1]); // D
+		vertex(this.points[4][0], this.points[4][1]); // E
+		endShape(CLOSE);
+
+		// Draws right rhombus
+		beginShape();
+		vertex(0, 0);								  // A
+		vertex(this.points[2][0], this.points[2][1]); // C
+		vertex(this.points[5][0], this.points[5][1]); // F
+		vertex(this.points[4][0], this.points[4][1]); // E
+		endShape(CLOSE);
+
+		pop();
 	}
-  
-	_rotatePoint(x, y, theta) {
-	  // x1 = x0cos(θ) – y0sin(θ)
-	  // y1 = x0sin(θ) + y0cos(θ)
+	
+	/*                       *
+	 *           G           *
+	 *        •     •        *
+	 *     B           C     *
+	 *     •  •     •  •     *
+	 *     •     A     •     *
+	 *     D     •     F     *
+	 *        •  •  •        *
+	 *           E           *
+	 *                       */
+	_buildPoints() {
+		let B = [
+			- this.edgeLength * Math.sin( this.viewAngle / 2 ), // Bx
+			- this.edgeLength * Math.cos( this.viewAngle / 2 )  // By
+		];
+
+		let C = [
+			+ this.edgeLength * Math.sin( this.viewAngle / 2 ), // Cx
+			- this.edgeLength * Math.cos( this.viewAngle / 2 )  // Cy
+		];
+
+		let D = [
+			- this.edgeLength * Math.sin( this.viewAngle / 2 ),     // Dx
+			this.edgeLength * (1 - Math.cos( this.viewAngle / 2 ))  // Dy
+		];
+
+		let E = [
+			+ 0, 			   // Ey
+			+ this.edgeLength  // Ex
+		];
+
+		let F = [
+			+ this.edgeLength * Math.sin( this.viewAngle / 2 ),     // Fx
+			this.edgeLength * (1 - Math.cos( this.viewAngle / 2 ))  // Fy
+		];
+
+		let G = [
+			+ 0,  //Gx
+			- 2 * this.edgeLength * Math.cos( this.viewAngle / 2 )
+		];
+
+		this.points.push(B, C, D, E, F, G);
 	}
-  }
+
+	get x() { return this.points[0][0]; }
+	get y() { return this.points[0][1]; }
   
+}
+  
+
+let test = new OrthoCube(200, 200, [0, 0], 120, 30);
 
 
 var x = 300;
@@ -29,32 +107,33 @@ var a = 100;
 var b = 100;
 // this is the fireworks example
 function draw_one_frame() {
-	//background(255);
-	x += 2;
-	y += 2;
-	a -= 2;
-	b -= 2;
-	strokeWeight(1);
-	translate(width / 2, height / 2);
-	for (var i = 0; i < 15; i++) {
-		for (var k = 0; k < 20; k++) {
-			stroke(255, 255, 255);
-			rotate(PI / 12.0);
-			fill(255, 255 - i * 10, 255 - k * 10);
-			line(a % 100, b % 100, x % 300, y % 300);
-			ellipse((x + i * 20) % width, (y + k * 20) % height, i + 4, i + 4);
-			drawtriangle((a - i * 20) % width, (b - k * 20) % height, k / 8);
-			rect(x % width, y % height, k + 10, k + 10);
-			fill(0, i * 10, 255 - k * 10);
-			ellipse((x - i * 20) % width, (y - k * 20) % height, i + 3, i + 3);
-			rotate(PI / 24.0);
-			fill(255 - (i + k) * 5, (i + k) * 7, i * 20);
-			drawtriangle((a + i * 20) % width, (b + k * 20) % height, k / 8);
-			rect(a % width, b % height, k + 5, k + 5);
-			drawflower(k, x);
-		}
-	}
+	test.draw();
 
+	//background(255);
+	// x += 2;
+	// y += 2;
+	// a -= 2;
+	// b -= 2;
+	// strokeWeight(1);
+	// translate(width / 2, height / 2);
+	// for (var i = 0; i < 15; i++) {
+	// 	for (var k = 0; k < 20; k++) {
+	// 		stroke(255, 255, 255);
+	// 		rotate(PI / 12.0);
+	// 		fill(255, 255 - i * 10, 255 - k * 10);
+	// 		line(a % 100, b % 100, x % 300, y % 300);
+	// 		ellipse((x + i * 20) % width, (y + k * 20) % height, i + 4, i + 4);
+	// 		drawtriangle((a - i * 20) % width, (b - k * 20) % height, k / 8);
+	// 		rect(x % width, y % height, k + 10, k + 10);
+	// 		fill(0, i * 10, 255 - k * 10);
+	// 		ellipse((x - i * 20) % width, (y - k * 20) % height, i + 3, i + 3);
+	// 		rotate(PI / 24.0);
+	// 		fill(255 - (i + k) * 5, (i + k) * 7, i * 20);
+	// 		drawtriangle((a + i * 20) % width, (b + k * 20) % height, k / 8);
+	// 		rect(a % width, b % height, k + 5, k + 5);
+	// 		drawflower(k, x);
+	// 	}
+	// }
 }
 
 function drawtriangle(x, y, r) {
