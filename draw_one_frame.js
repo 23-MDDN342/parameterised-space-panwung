da8be504623e56a25da63bcf9c769ac847557536
panwung <panwung@myvuw.ac.nz>
2024-02-29 21:02:34 +1300

    made grid class
    grid class draws a bunch of cubes objects and stores them in an array

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -98,68 +98,115 @@ class OrthoCube {
 	get y() { return this.points[0][1]; }
   
 }
-  
 
-const ANGLE = 120;
-const EDGE_LENGTH = 30;
-const SEPARATION = 3;
-let translationAngle = (Math.PI * (360 - 2 * ANGLE)) / 720;
-let translationX = EDGE_LENGTH * Math.cos(translationAngle);
-let translationY = EDGE_LENGTH * Math.sin(translationAngle);
+class CubeGrid {
+	constructor(x, y, maxRow, maxCol, viewAngleDeg, edgeLength, separation) {
+		this.x = x;
+		this.y = y;
 
+		let translationAngle = (Math.PI * (360 - 2 * viewAngleDeg)) / 720;
+		let translationX = (edgeLength + separation) * Math.cos(translationAngle);
+		let translationY = (edgeLength + separation) * Math.sin(translationAngle);
 
-let test1 = new OrthoCube(200, 200, undefined, ANGLE, EDGE_LENGTH);
-let test2 = new OrthoCube(200 + translationX + SEPARATION, 200 + translationY + SEPARATION, undefined, ANGLE, EDGE_LENGTH);
+		this.cubes = []; // 2D array of cubes
 
+		// even rows are 2row * translationX, odd rows are (2row + 1) * translationX
+		// even and odd cols are col * translationY
+		for (let col=0; col<maxCol; col++) {
+			let rowArray = [];
+			for (let row=0; row<maxRow; row++) {
+				let newX = x + ((col % 2 == 0) ? 2 * row * translationX : (2 * row + 1) * translationX);
+				let newY = y + col * translationY;
+				rowArray.push(new OrthoCube(newX, newY, [row, col], viewAngleDeg, edgeLength));
+			}
+			this.cubes.push(rowArray);
+		}
 
-var x = 300;
-var y = 300;
-var a = 100;
-var b = 100;
-// this is the fireworks example
-function draw_one_frame() {
-	test1.draw(255, 200, 100);
-	test2.draw(255, 200, 100);
+	}
 
-	//background(255);
-	// x += 2;
-	// y += 2;
-	// a -= 2;
-	// b -= 2;
-	// strokeWeight(1);
-	// translate(width / 2, height / 2);
-	// for (var i = 0; i < 15; i++) {
-	// 	for (var k = 0; k < 20; k++) {
-	// 		stroke(255, 255, 255);
-	// 		rotate(PI / 12.0);
-	// 		fill(255, 255 - i * 10, 255 - k * 10);
-	// 		line(a % 100, b % 100, x % 300, y % 300);
-	// 		ellipse((x + i * 20) % width, (y + k * 20) % height, i + 4, i + 4);
-	// 		drawtriangle((a - i * 20) % width, (b - k * 20) % height, k / 8);
-	// 		rect(x % width, y % height, k + 10, k + 10);
-	// 		fill(0, i * 10, 255 - k * 10);
-	// 		ellipse((x - i * 20) % width, (y - k * 20) % height, i + 3, i + 3);
-	// 		rotate(PI / 24.0);
-	// 		fill(255 - (i + k) * 5, (i + k) * 7, i * 20);
-	// 		drawtriangle((a + i * 20) % width, (b + k * 20) % height, k / 8);
-	// 		rect(a % width, b % height, k + 5, k + 5);
-	// 		drawflower(k, x);
-	// 	}
-	// }
-}
+	draw() {
+		for (let col=0; col<this.cubes.length; col++) {
+			for (let row=0; row<this.cubes[0].length; row++) {
+				this.cubes[col][row].draw((col % 2 == 0) ? [255, 0, 0] : [0, 255, 0], 120, 0);
+			}
+		}
+	}
 
-function drawtriangle(x, y, r) {
-	triangle(x, y, x + 7 * r, y - 13.75 * r, x + 14 * r, y);
-}
-
-function drawflower(i, k) {
-	if (i % 2 == 1) {
-		fill(255, (k * 0.4) % 255, 30);
-		stroke(k % 255, 255, 0);
-		arc(0, 0, 150 + i + 150 * sin(k * PI / 24), 150, 0, PI / 40);
-	} else {
-		fill(k % 255, 0, 255);
-		stroke(0, (k * 0.4) % 255, 255);
-		arc(0, 0, (100 + 100 * cos(k * PI / 24)) % 255, 50, 0, PI / 20);
+	setActiveCube() {
+		
 	}
 }
+
+
+
+
+
+const ANGLE = 120;
+const EDGE_LENGTH = 35;
+const SEPARATION = 3;
+const ROW_COUNT = 15;
+const COL_COUNT = 30;
+
+const grid = new CubeGrid(0, 0, ROW_COUNT, COL_COUNT, ANGLE, EDGE_LENGTH, SEPARATION);
+
+
+
+
+function draw_one_frame() {
+	grid.draw();
+}
+
+
+
+
+
+// var x = 300;
+// var y = 300;
+// var a = 100;
+// var b = 100;
+
+// // this is the fireworks example
+// function legacy() {
+// 	background(255);
+// 	x += 2;
+// 	y += 2;
+// 	a -= 2;
+// 	b -= 2;
+// 	strokeWeight(1);
+// 	translate(width / 2, height / 2);
+// 	for (var i = 0; i < 15; i++) {
+// 		for (var k = 0; k < 20; k++) {
+// 			stroke(255, 255, 255);
+// 			rotate(PI / 12.0);
+// 			fill(255, 255 - i * 10, 255 - k * 10);
+// 			line(a % 100, b % 100, x % 300, y % 300);
+// 			ellipse((x + i * 20) % width, (y + k * 20) % height, i + 4, i + 4);
+// 			drawtriangle((a - i * 20) % width, (b - k * 20) % height, k / 8);
+// 			rect(x % width, y % height, k + 10, k + 10);
+// 			fill(0, i * 10, 255 - k * 10);
+// 			ellipse((x - i * 20) % width, (y - k * 20) % height, i + 3, i + 3);
+// 			rotate(PI / 24.0);
+// 			fill(255 - (i + k) * 5, (i + k) * 7, i * 20);
+// 			drawtriangle((a + i * 20) % width, (b + k * 20) % height, k / 8);
+// 			rect(a % width, b % height, k + 5, k + 5);
+// 			drawflower(k, x);
+// 		}
+// 	}
+// }
+
+
+// function drawtriangle(x, y, r) {
+// 	triangle(x, y, x + 7 * r, y - 13.75 * r, x + 14 * r, y);
+// }
+
+// function drawflower(i, k) {
+// 	if (i % 2 == 1) {
+// 		fill(255, (k * 0.4) % 255, 30);
+// 		stroke(k % 255, 255, 0);
+// 		arc(0, 0, 150 + i + 150 * sin(k * PI / 24), 150, 0, PI / 40);
+// 	} else {
+// 		fill(k % 255, 0, 255);
+// 		stroke(0, (k * 0.4) % 255, 255);
+// 		arc(0, 0, (100 + 100 * cos(k * PI / 24)) % 255, 50, 0, PI / 20);
+// 	}
+// }
