e832cb922694b9292be2d19b4f529e1e408563d1
panwung <panwung@myvuw.ac.nz>
2024-03-02 15:47:28 +1300

    fixed the issues ?
    the cubes now dont fight over who affects what, and the cubes also average their height

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -115,6 +115,8 @@ class OrthoCube {
 
 class CubeGrid {
 	constructor(x, y, maxRow, maxCol, viewAngleDeg, edgeLength, separation, maxRaiseHeight, raiseRadius) {
+		const SPEED = 1;
+
 		// x and y values for the very top cube
 		this.x = x;
 		this.y = y;
@@ -131,7 +133,6 @@ class CubeGrid {
 
 		this.cubes = []; // 2D array of cubes
 		this.edgeCubes = []; // Array of edge cubes
-		this.raisedCubes = []; // Array of raised cubes
 
 		for (let col=0; col<maxCol; col++) {
 			let rowArray = [];
@@ -151,19 +152,19 @@ class CubeGrid {
 					!(row === maxRow - 1 && col === maxCol - 1)
 				) {
 					if (row === 0) {
-						newCube.propagationDir = [+1, 0];
+						newCube.propagationDir = [+SPEED, 0];
 						this.edgeCubes.push(newCube); 
 					}
 					else if (row === maxRow - 1) {
-						newCube.propagationDir = [-1, 0];
+						newCube.propagationDir = [-SPEED, 0];
 						this.edgeCubes.push(newCube);
 					}
 					else if (col === 0) {
-						newCube.propagationDir = [0, +1]
+						newCube.propagationDir = [0, +SPEED]
 						this.edgeCubes.push(newCube);
 					}
 					else if (col === maxCol - 1) {
-						newCube.propagationDir = [0, -1]
+						newCube.propagationDir = [0, -SPEED]
 						this.edgeCubes.push(newCube);
 					}
 				}
@@ -257,23 +258,11 @@ class CubeGrid {
 	raiseAdjacentCubes(activeCube) {
 		let adjCubes = this.getAdjacentCubes(activeCube);
 
-		// this causes problems with multiple cubes
-		for (let cube of this.raisedCubes) {
-			if (!adjCubes.includes(cube) && !cube.active) { cube.raiseHeight = 0; }
-		}
-		
-		this.raisedCubes = [];
-
 		for (let cube of adjCubes) {
 			let range = Math.floor( Math.sqrt( Math.abs(cube.row - activeCube.row) ** 2 + Math.abs(cube.col - activeCube.col) ** 2 ) );
-			// cube.raiseHeight = (this.maxRaiseHeight / (range + 1));
-			
-			cube.raiseHeight = ((this.raiseRadius - range) * this.maxRaiseHeight / this.raiseRadius);
+			cube.raiseHeight = ( ((this.raiseRadius - range) * this.maxRaiseHeight / this.raiseRadius) + cube.raiseHeight ) / 2;
 		}
 
-		for (let cube of adjCubes) {
-			this.raisedCubes.push(cube);
-		}
 
 
 		// for (let cube of adjCubes) {
@@ -346,9 +335,6 @@ class CubeGrid {
 }
 
 
-
-
-
 const ANGLE = 120;
 const EDGE_LENGTH = canvasHeight/6;
 const SEPARATION = 5;
@@ -364,63 +350,7 @@ grid.setActiveRandomEdgeCube();
 
 let test = [1, 2, 3, 4]
 
-
 function draw_one_frame() {
 	grid.draw([0, 255, 255], [255, 0, 0]);
 	grid.propagateActiveCubes();
 }
-
-
-
-
-
-// var x = 300;
-// var y = 300;
-// var a = 100;
-// var b = 100;
-
-// // this is the fireworks example
-// function legacy() {
-// 	background(255);
-// 	x += 2;
-// 	y += 2;
-// 	a -= 2;
-// 	b -= 2;
-// 	strokeWeight(1);
-// 	translate(width / 2, height / 2);
-// 	for (var i = 0; i < 15; i++) {
-// 		for (var k = 0; k < 20; k++) {
-// 			stroke(255, 255, 255);
-// 			rotate(PI / 12.0);
-// 			fill(255, 255 - i * 10, 255 - k * 10);
-// 			line(a % 100, b % 100, x % 300, y % 300);
-// 			ellipse((x + i * 20) % width, (y + k * 20) % height, i + 4, i + 4);
-// 			drawtriangle((a - i * 20) % width, (b - k * 20) % height, k / 8);
-// 			rect(x % width, y % height, k + 10, k + 10);
-// 			fill(0, i * 10, 255 - k * 10);
-// 			ellipse((x - i * 20) % width, (y - k * 20) % height, i + 3, i + 3);
-// 			rotate(PI / 24.0);
-// 			fill(255 - (i + k) * 5, (i + k) * 7, i * 20);
-// 			drawtriangle((a + i * 20) % width, (b + k * 20) % height, k / 8);
-// 			rect(a % width, b % height, k + 5, k + 5);
-// 			drawflower(k, x);
-// 		}
-// 	}
-// }
-
-
-// function drawtriangle(x, y, r) {
-// 	triangle(x, y, x + 7 * r, y - 13.75 * r, x + 14 * r, y);
-// }
-
-// function drawflower(i, k) {
-// 	if (i % 2 == 1) {
-// 		fill(255, (k * 0.4) % 255, 30);
-// 		stroke(k % 255, 255, 0);
-// 		arc(0, 0, 150 + i + 150 * sin(k * PI / 24), 150, 0, PI / 40);
-// 	} else {
-// 		fill(k % 255, 0, 255);
-// 		stroke(0, (k * 0.4) % 255, 255);
-// 		arc(0, 0, (100 + 100 * cos(k * PI / 24)) % 255, 50, 0, PI / 20);
-// 	}
-// }
