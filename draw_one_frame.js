a941c3acc22b091da1bfd4f61c78508058143231
panwung <panwung@myvuw.ac.nz>
2024-03-02 15:18:33 +1300

    half made the proportional moving
    needs a lot of work, especially near the edge. it also looks far more square than circular, may need to redo the selection method

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -14,10 +14,12 @@ class OrthoCube {
 
 		this.active = active;
 
+		this.raiseHeight = 0;
+
 		this._initPoints();
 	}
   
-	draw(cubeCol, raiseHeight=0) {
+	draw(cubeCol) {
 		push();
 		noStroke();
 		translate(this.points[0][0], this.points[0][1]);
@@ -25,28 +27,28 @@ class OrthoCube {
 		// Draws top rhombus
 		fill(this._colorBrightness(cubeCol, 1));
 		beginShape();
-		vertex(0, - raiseHeight);						  			// A
-		vertex(this.points[1][0], this.points[1][1] - raiseHeight); // B
-		vertex(this.points[6][0], this.points[6][1] - raiseHeight); // G
-		vertex(this.points[2][0], this.points[2][1] - raiseHeight); // C
+		vertex(0, - this.raiseHeight);						  		     // A
+		vertex(this.points[1][0], this.points[1][1] - this.raiseHeight); // B
+		vertex(this.points[6][0], this.points[6][1] - this.raiseHeight); // G
+		vertex(this.points[2][0], this.points[2][1] - this.raiseHeight); // C
 		endShape(CLOSE);
 
 		// Draws left rhombus
 		fill(this._colorBrightness(cubeCol, 2/3));
 		beginShape();
-		vertex(0, - raiseHeight);						 			// A
-		vertex(this.points[1][0], this.points[1][1] - raiseHeight); // B
-		vertex(this.points[3][0], this.points[3][1] - raiseHeight); // D
-		vertex(this.points[4][0], this.points[4][1] - raiseHeight); // E
+		vertex(0, - this.raiseHeight);						 			 // A
+		vertex(this.points[1][0], this.points[1][1] - this.raiseHeight); // B
+		vertex(this.points[3][0], this.points[3][1] - this.raiseHeight); // D
+		vertex(this.points[4][0], this.points[4][1] - this.raiseHeight); // E
 		endShape(CLOSE);
 
 		// Draws right rhombus
 		fill(this._colorBrightness(cubeCol, 1/3));
 		beginShape();
-		vertex(0, - raiseHeight);								    // A
-		vertex(this.points[2][0], this.points[2][1] - raiseHeight); // C
-		vertex(this.points[5][0], this.points[5][1] - raiseHeight); // F
-		vertex(this.points[4][0], this.points[4][1] - raiseHeight); // E
+		vertex(0, - this.raiseHeight);								     // A
+		vertex(this.points[2][0], this.points[2][1] - this.raiseHeight); // C
+		vertex(this.points[5][0], this.points[5][1] - this.raiseHeight); // F
+		vertex(this.points[4][0], this.points[4][1] - this.raiseHeight); // E
 		endShape(CLOSE);
 
 		pop();
@@ -112,23 +114,28 @@ class OrthoCube {
 }
 
 class CubeGrid {
-	constructor(x, y, maxRow, maxCol, viewAngleDeg, edgeLength, separation, maxRaiseHeight) {
+	constructor(x, y, maxRow, maxCol, viewAngleDeg, edgeLength, separation, maxRaiseHeight, raiseRadius) {
+		// x and y values for the very top cube
 		this.x = x;
 		this.y = y;
+
+		// Maximum raise height for active cubes
 		this.maxRaiseHeight = maxRaiseHeight;
+
+		// Radius of influence of active cubes 
+		this.raiseRadius = raiseRadius;
 
 		let translationAngle = (Math.PI * (360 - 2 * viewAngleDeg)) / 720;
 		let translationX = (edgeLength + separation) * Math.cos(translationAngle);
 		let translationY = (edgeLength + separation) * Math.sin(translationAngle);
 
 		this.cubes = []; // 2D array of cubes
-		this.edgeCubes = [];
+		this.edgeCubes = []; // Array of edge cubes
+		this.raisedCubes = []; // Array of raised cubes
 
 		for (let col=0; col<maxCol; col++) {
 			let rowArray = [];
 			for (let row=0; row<maxRow; row++) {
-				// let newX = x + ((col % 2 == 0) ? 2 * row * translationX : (2 * row + 1) * translationX);
-				// let newY = y + col * translationY;
 
 				let newX = x + row * translationX;
 				let newY = y + row * translationY;
@@ -167,23 +174,15 @@ class CubeGrid {
 		}
 	}
 
-	draw() {
+	draw(passiveCol, activeCol) {
 		for (let col=0; col<this.cubes.length; col++) {
 			for (let row=0; row<this.cubes[0].length; row++) {
 				let cube = this.cubes[col][row];
-				cube.draw((cube.active) ? [255, 0, 0] : [0, 255, 255], (cube.active === true) ? this.maxRaiseHeight : 0);
+				cube.draw((cube.active) ? activeCol : passiveCol);
 			}
 		}
 	}
 
-	/**
-	 * Grab an activeCube
-	 * Figure out the nextCube based on the propagationDir
-	 * Set the nextCube to active and set its propigation to the activeCube's
-	 * Set activeCube.active to false
-	 * Set activeCube.propagationDir to undefined (unless it is in this.edgeCubes)
-	 * Add nextCube to an array to exclude from search of activeCubes
-	 */
 	propagateActiveCubes() {
 		let excludeFromSearch = [];
 
@@ -213,9 +212,15 @@ class CubeGrid {
 						nextCube.active = true;
 						if (!this.edgeCubes.includes(nextCube)) nextCube.propagationDir = activeCube.propagationDir;
 
+						// Sets the raise height of the next cube to max and resets the active height
+						activeCube.raiseHeight = 0;
+						nextCube.raiseHeight = this.maxRaiseHeight;
+
 						// Exclude it from search
 						excludeFromSearch.push(nextCube);
 
+						// Radiually influence the raise height of adjacent cubes
+						this.raiseAdjacentCubes(nextCube);
 					}
 					
 					// Set the active cube to false and reset its propagation, unless it is an edge cube
@@ -224,6 +229,85 @@ class CubeGrid {
 				}
 			}
 		}
+	}
+
+	getAdjacentCubes(activeCube) {
+		let adjCubes = [];
+		for (let col=-this.raiseRadius; col<=this.raiseRadius; col++) {
+			for (let row=-this.raiseRadius; row<=this.raiseRadius; row++) {
+
+				let checkRow = activeCube.row + row;
+				let checkCol = activeCube.col + col;
+				// Checks if this particular cube is in bounds, ignoring the active
+				if (
+					checkRow >= 0 && checkRow < this.cubes[0].length &&
+					checkCol >= 0 && checkCol < this.cubes.length &&
+					!(row === 0 && col === 0)
+				) { adjCubes.push(this.cubes[checkCol][checkRow]); }
+			}
+		}
+		return adjCubes;
+	}
+
+	raiseAdjacentCubes(activeCube) {
+		let adjCubes = this.getAdjacentCubes(activeCube);
+
+		for (let cube of this.raisedCubes) {
+			if (!adjCubes.includes(cube) && !cube.active) { cube.raiseHeight = 0; }
+		}
+		this.raisedCubes = [];
+
+		for (let cube of adjCubes) {
+			let range = Math.floor( Math.sqrt( Math.abs(cube.row - activeCube.row) ** 2 + Math.abs(cube.col - activeCube.col) ** 2 ) );
+			cube.raiseHeight = (this.maxRaiseHeight / (range + 1));
+		}
+
+		for (let cube of adjCubes) {
+			this.raisedCubes.push(cube);
+		}
+
+
+		// for (let cube of adjCubes) {
+		// 	if (adjCubes.includes())
+		// }
+
+
+
+		// for (let col=-this.raiseRadius; col<=this.raiseRadius; col++) {
+		// 	for (let row=-this.raiseRadius; row<=this.raiseRadius; row++) {
+
+		// 		let checkRow = activeCube.row + row;
+		// 		let checkCol = activeCube.col + col;
+		// 		// Checks if this particular cube is in bounds
+		// 		if (
+		// 			checkRow >= 0 && checkRow < this.cubes[0].length &&
+		// 			checkCol >= 0 && checkCol < this.cubes.length &&
+		// 			!(row === 0 && col === 0)
+		// 		) {
+
+
+
+					// let cube = this.cubes[checkCol][checkRow];
+					
+					// let range = Math.ceil(Math.sqrt( Math.abs(row) ** 2 + Math.abs(col) ** 2 ));
+
+
+					// this is not working because when the actice cube mioves over the cubes left behind are no longer in range of hte loops
+					// consider adding all of the raised cubes to an array and the next time the acitve cube moves over,
+					// compare the current list to the previous list
+					// if (range > this.raiseRadius) { cube.raiseHeight = 0; console.log("CISFABFKSB") }
+					// console.log(range)
+					// cube.raiseHeight = ((this.maxRaiseHeight / (range + 1)) + cube.raiseHeight)/2;
+
+
+					// cube.raiseHeight = this.maxRaiseHeight / (range + 1)
+					
+						
+					// }
+					// else {  }
+				// }
+			// }
+		// }
 	}
 
 	toggleCubeActive(row, col) {
@@ -241,6 +325,15 @@ class CubeGrid {
 		console.log(edgeCube.gridPos);
 		edgeCube.active = true;
 	}
+	
+	getAllActive() {
+		for (let col=0; col<this.cubes.length; col++) {
+			for (let row=0; row<this.cubes[0].length; row++) {
+				let activeCube = this.cubes[col][row];
+				if (activeCube.active) console.log("POS: (" + activeCube.gridPos + "), DIR: (" + activeCube.propagationDir + ")");
+			}
+		}
+	}
 }
 
 
@@ -248,24 +341,24 @@ class CubeGrid {
 
 
 const ANGLE = 120;
-const EDGE_LENGTH = canvasHeight/9;
-const SEPARATION = 7;
-const ROW_COUNT = 23;
-const COL_COUNT = 23;
-const RAISE_HEIGHT = 20;
+const EDGE_LENGTH = canvasHeight/6;
+const SEPARATION = 5;
 
-const grid = new CubeGrid(canvasWidth/2, -canvasHeight, ROW_COUNT, COL_COUNT, ANGLE, EDGE_LENGTH, SEPARATION, RAISE_HEIGHT);
+// 13x13 is perfect for rebounding, 24x24 is perfect for linear
+const ROW_COUNT = 13; 
+const COL_COUNT = 13; 
+const MAX_RAISE_HEIGHT = EDGE_LENGTH;
+const RAISE_RADIUS = 4;
+
+const grid = new CubeGrid(canvasWidth/2, -canvasHeight/2, ROW_COUNT, COL_COUNT, ANGLE, EDGE_LENGTH, SEPARATION, MAX_RAISE_HEIGHT, RAISE_RADIUS);
+grid.setActiveRandomEdgeCube();
 
 let test = [1, 2, 3, 4]
 
 
 function draw_one_frame() {
-	
-	grid.draw();
+	grid.draw([0, 255, 255], [255, 0, 0]);
 	grid.propagateActiveCubes();
-	
-	// noLoop();
-	
 }
 
 
